9c48ae56d6688ae7b44dbc23054fa0503cbe03a0
Luke Kellet <lukejameskellet@hotmail.com>
2023-10-11 11:14:58 +0100

    Add project files.

---
+++ AngularApp/Scripts/myapp.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('MyApp', []);
+app.controller('MyController', function ($scope) {
+    $scope.myVal = "Welcome to Angular.JS";
+
+    $scope.MyFun = function () {
+        alert("Hello world");
+    }
+    $scope.MyFun2 = function (val) {
+        alert("Parameter passed: " + val);
+    }
+});
